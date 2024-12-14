9ac60fa70ebcd1d97fb71ebf3fda1f068866501b
SlavikGemay <gemayit@gmail.com>
2024-12-14 14:06:45 +0200

    4.2 update

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 'use strict';
 
-const threeDigitNumberInput = prompt('Enter three-digit number')
+const threeDigitNumberInput = prompt('Enter three-digit number');
+const str = 123;
+const strNum = String(str);
 
 if (!threeDigitNumberInput?.trim() || isNaN(threeDigitNumberInput)
     || !Number.isInteger(+threeDigitNumberInput)) {
@@ -8,8 +10,15 @@ if (!threeDigitNumberInput?.trim() || isNaN(threeDigitNumberInput)
 }
 else {
     const num = Math.abs(+threeDigitNumberInput);
-    if (String(num).length === 3);
+    if (String(num).length === 3) {} // отак можно оставить или необходимо перенесту скобку вторую ниже?
 else {
         alert('Number is not three-digit')
     }
+}
+const digit1 = strNum[0];
+const digit2 = strNum[1];
+const digit3 = strNum[2];
+
+if (digit1 === digit1 || digit1 === digit2 || digit1 === digit3) {
+    alert('You entered 2 or 3 identical numbers');
 }
